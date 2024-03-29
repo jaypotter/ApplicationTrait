6f6a1d328b0ef23f68201fcaddfb387cfd462d39
Jay Potter <j@ypotter.ca>
2024-03-29 11:16:50 -0400

    Create ApplicationTrait.php

---
+++ src/Potter/Application/ApplicationTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Application;
+
+use \Potter\{
+    Container\ContainerInterface,
+    Session\SessionInterface
+};
+
+trait ApplicationTrait 
+{
+    final public function getSession(): SessionInterface
+    {
+        return $this->getContainer()->get('session');
+    }
+    
+    final public function runningInConsole(): bool
+    {
+        return php_sapi_name() == 'cli';
+    }
+    
+    abstract public function getContainer(): ContainerInterface;
+}
